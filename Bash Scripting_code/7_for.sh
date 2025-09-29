436fe5d02fbd7a649ddab32d0d7d4337d99ad488
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-29 14:05:38 +0530

    update

---
+++ Bash Scripting_code/7_for.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for fruit in apple banana mongo papaya grapes
+do
+        echo "I like $fruit"
+
+done
+
+echo "##################"
+for ((i=1; i<=5; i++))
+do
+    echo "Count: $i"
+done
