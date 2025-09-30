4b7719329aa46c34c596c632e4acd062c0ddb70a
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-30 10:31:52 +0530

    update

---
+++ Bash Scripting_code/forloop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Numbers from 1 to 10:"
+for i in {1..10}
+do
+    echo "Number: $i"
+done
+
+echo ""
+echo "Files in current directory:"
+for file in *
+do
+    echo "File: $file"
+done
+
