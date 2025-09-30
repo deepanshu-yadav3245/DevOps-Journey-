f7e111e65496d20a4e155ef4d239343115f8c6b1
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-30 10:32:12 +0530

    update

---
+++ Bash Scripting_code/practice1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "##################################"
+
+echo "My name Deepanshu yadav"
+sleep 1
+
+echo "####################################"
+
+echo "I am student of BCA"
+sleep 1
+
+echo "####################################"
+
+echo "Currently Learning Devops"
+sleep 1
+
+echo "####################################"
+
+echo "Task Completed!"
